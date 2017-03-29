8083372a90f14a1334f1d464c85d1f0726d7b103
mgoodrich <mark.goodrich@live.com>
2017-03-29 17:07:49 -0400

    change lab tracking controller to populate fragment model, not page model

--- omod/src/main/java/org/openmrs/module/labtrackingapp/fragment/controller/LabtrackingPatientDashboardFragmentController.java
+++ omod/src/main/java/org/openmrs/module/labtrackingapp/fragment/controller/LabtrackingPatientDashboardFragmentController.java
@@ -5,14 +5,14 @@ import org.openmrs.module.appframework.domain.AppDescriptor;
 import org.openmrs.module.appui.UiSessionContext;
 import org.openmrs.module.labtrackingapp.api.LabTrackingAppService;
 import org.openmrs.ui.framework.annotation.SpringBean;
-import org.openmrs.ui.framework.page.PageModel;
+import org.openmrs.ui.framework.fragment.FragmentModel;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public class LabtrackingPatientDashboardFragmentController {
 
     private static final int MAX_ROWS_TO_SHOW = 5;
 
-    public Object controller(PageModel model, @RequestParam(value = "appId", required = false) AppDescriptor app,
+    public Object controller(FragmentModel model, @RequestParam(value = "appId", required = false) AppDescriptor app,
                              @RequestParam(value = "returnUrl", required = false) String returnUrl,
                              @RequestParam(value = "patientId", required = false) Patient patient,
                              @SpringBean("labtrackingapp.LabTrackingAppService") LabTrackingAppService labTrackingAppService,
