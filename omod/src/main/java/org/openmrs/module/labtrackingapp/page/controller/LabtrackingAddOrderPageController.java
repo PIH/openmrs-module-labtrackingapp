89a670b5ab5539ac8404d7bd073e32bda262e051
Cosmin Ioan <cioan@pih.org>
2022-07-18 14:53:41 -0400

    UHM-6418, edit pathology order from the visit dashboard

--- omod/src/main/java/org/openmrs/module/labtrackingapp/page/controller/LabtrackingAddOrderPageController.java
+++ omod/src/main/java/org/openmrs/module/labtrackingapp/page/controller/LabtrackingAddOrderPageController.java
@@ -1,17 +1,22 @@
 package org.openmrs.module.labtrackingapp.page.controller;
 
+import org.apache.commons.lang.StringUtils;
 import org.joda.time.DateTime;
+import org.openmrs.Order;
 import org.openmrs.Patient;
 import org.openmrs.Visit;
+import org.openmrs.Encounter;
 import org.openmrs.module.appframework.domain.AppDescriptor;
 import org.openmrs.module.appui.UiSessionContext;
 import org.openmrs.module.emrapi.patient.PatientDomainWrapper;
+import org.openmrs.module.labtrackingapp.LabTrackingConstants;
 import org.openmrs.ui.framework.annotation.InjectBeans;
 import org.openmrs.ui.framework.page.PageModel;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Set;
 import java.util.TimeZone;
 
 public class LabtrackingAddOrderPageController {
@@ -21,6 +26,7 @@ public class LabtrackingAddOrderPageController {
                              @RequestParam(value = "visitId", required = false) Visit visit,
                              @RequestParam(value = "patientId") Patient patient,
                              @RequestParam(value = "orderUuid", required = false) String orderUuid,
+                             @RequestParam(value = "encounter", required = false) Encounter encounter,
                              @InjectBeans PatientDomainWrapper patientDomainWrapper,
                              UiSessionContext uiSessionContext) {
 
@@ -45,6 +51,20 @@ public class LabtrackingAddOrderPageController {
         }
         model.addAttribute("visitStartDateTime", zonedVisitStartDateTime);
         model.addAttribute("visitEndDateTime", zonedVisitEndDateTime);
+        if ( StringUtils.isBlank(orderUuid) && encounter !=null) {
+            Set<Order> orders = encounter.getOrders();
+            if (orders != null && orders.size() > 0) {
+                for (Order order : orders) {
+                    // looking for first PATHOLOGY_ORDER activated at the same time as the encounter
+                    if (StringUtils.equalsIgnoreCase(order.getOrderType().getUuid(), LabTrackingConstants.LAB_TRACKING_PATHOLOGY_ORDER_TYPE_UUID)) {
+                        if (order.getDateActivated().equals(encounter.getEncounterDatetime())) {
+                            orderUuid = order.getUuid();
+                            break;
+                        }
+                    }
+                }
+            }
+        }
         model.addAttribute("orderUuid", orderUuid);
         model.addAttribute("serverDatetime", new DateTime()); // just in case the server and client time are not in sync
 
